14a6ed4c6ab85b727414db534dcc7c00eeb9e563
Robz8 <bobesh8@gmail.com>
2016-10-22 12:18:47 -0600

    Use ROMFS

--- BuildCIA.cmd
+++ BuildCIA.cmd
@@ -1,4 +1,4 @@
 @echo off
 tools\bannertool makebanner -i "app\banner.png" -ca "app\banneraudio.bcwav" -o "app\banner.bin"
-tools\makerom -f cia -target t -exefslogo -o TWLoader.cia -elf twlNANDfiler.elf -rsf app\build-cia.rsf -banner app\banner.bin -icon app\icon.bin -major 1 -minor 1
+tools\makerom -f cia -target t -exefslogo -o TWLoader.cia -elf twlNANDfiler.elf -rsf app\build-cia.rsf -ROMFS_ROOT="romfs" -banner app\banner.bin -icon app\icon.bin -major 1 -minor 1
 pause
