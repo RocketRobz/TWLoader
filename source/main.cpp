4fd8e69f2e6ba1271a2651a3b479cffc8fa1b13a
billy-acuna <armandoacuna.audiorama@gmail.com>
2016-10-29 12:18:04 -0700

    Fix APT's strings...
    in order to work with lastest ctrulib

--- source/main.cpp
+++ source/main.cpp
@@ -26,11 +26,12 @@ int main()
 	gfxInitDefault(); // Init graphic stuff
 
 	// We need these 2 buffers for APT_DoAppJump() later. They can be smaller too
-	u8 buf0[0x300];
-	u8 buf1[0x20];
+	u8 param[0x300];
+	u8 hmac[0x20];
 	// Clear both buffers
-	memset(buf0, 0, 0x300);
-	memset(buf1, 0, 0x20);
+	memset(param, 0, sizeof(param));
+	memset(hmac, 0, sizeof(hmac));
+			
 
 	// Loop as long as the status is not exit
 	while(aptMainLoop()) {
@@ -115,15 +116,11 @@ int main()
 			bootstrapini.SetString("NDS-BOOTSTRAP", "NDS_PATH",fat+rom);
 			bootstrapini.SaveIniFile( "sdmc:/_nds/nds-bootstrap.ini");
 
-			// Open an APT session so we can talk to the APT service
-			aptOpenSession();
 			// Prepare for the app launch
-			APT_PrepareToDoAppJump(0, 0x000480054B425345LL, 0); // bootstrap title ID
+			APT_PrepareToDoApplicationJump(0, 0x000480054B425345LL, 0); // bootstrap title ID
 			// Tell APT to trigger the app launch and set the status of this app to exit
-			APT_DoAppJump(0x300 /* size of buf0 */, 0x20 /* size of buf1 */, buf0, buf1);
-			// Close the APT session because we don't need APT anymore
-			aptCloseSession();
-		}
+			APT_DoApplicationJump(param, sizeof(param), hmac);
+			}
 
 		// Flush + swap framebuffers and wait for VBlank. Not really needed in this example
 		gfxFlushBuffers();
