ca25fae6179799adcc41cf49d9d8da9416c70a84
Robz8 <bobesh8@gmail.com>
2016-12-24 22:50:01 -0700

    Multiple support for both retail and homebrew ROMs

--- twlapp/arm9/source/main.cpp
+++ twlapp/arm9/source/main.cpp
@@ -116,7 +116,7 @@ int main(int argc, char **argv) {
 	bool HealthandSafety_MSG = false;
 	bool UseNTRSplash = true;
 	bool TriggerExit = false;
-	std::string	bootstrapPath = "";
+	// std::string	bootstrapPath = "";
 
 	bool consoleOn = false;
 
@@ -126,7 +126,7 @@ int main(int argc, char **argv) {
 	if (fatInitDefault()) {
 		CIniFile twloaderini( "sd:/_nds/twloader/settings.ini" );
 		
-		bootstrapPath = twloaderini.GetString( "TWL-MODE", "BOOTSTRAP_INI", "");
+		// bootstrapPath = twloaderini.GetString( "TWL-MODE", "BOOTSTRAP_INI", "");
 		
 		if(twloaderini.GetInt("TWL-MODE","HEALTH&SAFETY_MSG",0) == 1) { HealthandSafety_MSG = true; }
 		if(twloaderini.GetInt("TWL-MODE","TWL_CLOCK",0) == 1) { UseNTRSplash = false; }
@@ -231,6 +231,7 @@ int main(int argc, char **argv) {
 	defaultExceptionHandler();
 
 	std::string filename;
+	std::string gamename;
 
 	if (!fatInitDefault()) {
 		// Subscreen as a console
@@ -248,6 +249,7 @@ int main(int argc, char **argv) {
 	vector<string> extensionList;
 	extensionList.push_back(".nds");
 	extensionList.push_back(".argv");
+	FILE* ndsfile;
 
 	while(1) {
 
@@ -256,10 +258,27 @@ int main(int argc, char **argv) {
 		break;
 		}
 		
+		struct sNDSHeadersmall {
+			char gameTitle[12];			//!< 12 characters for the game title.
+			char gameCode[4];			//!< 4 characters for the game code.
+			u16 makercode;			//!< identifies the (commercial) developer.
+		} NDSHeader;
+		
 		CIniFile bootstrapini( "sd:/_nds/nds-bootstrap.ini" );
-		filename = bootstrapini.GetString("NDS-BOOTSTRAP", "BOOTSTRAP_PATH","");
-		filename = ReplaceAll( filename, "fat:/", "sd:/");
-		runFile(filename);
+		gamename = bootstrapini.GetString("NDS-BOOTSTRAP", "NDS_PATH","");
+		gamename = ReplaceAll( gamename, "fat:/", "sd:/");
+		ndsfile = fopen(gamename.c_str(),"rb");
+		fread(&NDSHeader,1,sizeof(NDSHeader),ndsfile);
+		
+		if (NDSHeader.makercode != 0x0000) {	// if the launched game is a retail ROM
+			filename = bootstrapini.GetString("NDS-BOOTSTRAP", "BOOTSTRAP_PATH","");
+			filename = ReplaceAll( filename, "fat:/", "sd:/");
+			runFile(filename);
+		} else {	// or if the launched game is a homebrew ROM
+			filename = bootstrapini.GetString("NDS-BOOTSTRAP", "BOOTSTRAP_PATH_HB","");
+			filename = ReplaceAll( filename, "fat:/", "sd:/");
+			runFile(filename);
+		}
 		
 		// Subscreen as a console
 		videoSetModeSub(MODE_0_2D);
