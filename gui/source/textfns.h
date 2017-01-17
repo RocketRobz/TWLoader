f5815849023421569407fdd76ad52c0cd225accd
David Korth <gerbilsoft@gerbilsoft.com>
2017-01-16 21:26:11 -0500

    utf8_to_wchar(): New version of utf8_to_wstring() that returns a malloc()'d wchar_t*.
    utf8_to_wstring() has been refactored to use an internal function that's
    also shared with utf8_to_wchar().

--- gui/source/textfns.h
+++ gui/source/textfns.h
@@ -11,4 +11,11 @@
  */
 std::wstring utf8_to_wstring(const char *str);
 
+/**
+ * Convert a UTF-8 string to wchar_t*.
+ * @param str UTF-8 string.
+ * @return malloc()'d wchar_t*. (UTF-32) (NOTE: If str is nullptr, this returns nullptr.)
+ */
+wchar_t *utf8_to_wchar(const char *str);
+
 #endif /* TEXTFNS_H */
