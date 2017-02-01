978b24d8ba76295dbe5b088e0fd9d4badb627cea
Jolty <juplagon@gmail.com>
2017-02-01 14:07:10 +0100

    twlnand-side compile.bat: create cia folder
    Create that directory

--- compile.bat
+++ compile.bat
@@ -3,6 +3,7 @@ cd twlnand-side
 make
 patch_ndsheader_dsiware.py TWLapp-hb.nds --mode dsi --maker 01 --code TWLD --title "TWLOADER-TWL" --out TWLapp.nds
 make_cia --srl="TWLapp.nds"
+mkdir "../7zfile/sdroot/_nds/twloader/cia/"
 copy "TWLapp.cia" "../7zfile/sdroot/_nds/twloader/cia/TWLoader - TWLNAND side.cia"
 cd ..
 cd NTR_Launcher
