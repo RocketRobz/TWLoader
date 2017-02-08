0c491ec4142573cccb3e81cbea88b95c129a1a7e
Jolty <juplagon@gmail.com>
2017-02-08 13:06:47 +0100

    download.cpp: fix the problem that always TWLoader wants to autodownload new version
    No more problems with autodownload.

--- gui/source/download.cpp
+++ gui/source/download.cpp
@@ -41,10 +41,6 @@ extern int screenmode;
 // 0: ROM select
 // 1: Settings
 
-typedef struct {
-	char text[13];
-} sVerfile;
-
 extern char settings_vertext[13];
 extern char settings_latestvertext[13];
 
@@ -192,14 +188,29 @@ int checkUpdate(void) {
 	int res = downloadFile(DOWNLOAD_VER_URL, "/_nds/twloader/ver", MEDIA_SD_FILE);
 	if (logEnabled)	LogFM("checkUpdate", "downloadFile() end");
 	if (res == 0) {
-		sVerfile Verfile;
 
 		bool isUnknown = false;
 		FILE* VerFile = fopen("sdmc:/_nds/twloader/ver", "r");
 		if (VerFile) {
-			fread(&Verfile, 1, sizeof(Verfile), VerFile);
-			strcpy(settings_latestvertext, Verfile.text);
+			/** This is the same method as checkBootstrapVersion() **/
+			long fileSize;
+			
+			fseek(VerFile , 0 , SEEK_END);
+			fileSize = ftell(VerFile);
+			
+			char buf[fileSize + 1];
+
+			// Clean buf array
+			for (size_t i=0; i< sizeof(buf); i++){
+				buf[i] = '\0';
+			}	
+			
+			rewind(VerFile);
+			fread(buf,1,sizeof(settings_latestvertext),VerFile);
+			buf[sizeof(buf) - 1] = '\0';
+			strcpy(settings_latestvertext, buf);
 			fclose(VerFile);
+			/** End **/
 		} else {
 			// Unable to open the version file.
 			strcpy(settings_latestvertext, "Unknown");
@@ -214,7 +225,7 @@ int checkUpdate(void) {
 		}
 		sf2d_draw_texture(dialogboxtex, 0, 0);
 		if (!isUnknown && !strcmp(settings_latestvertext, settings_vertext)) {
-			// Version is different.
+			// Version is not different.
 			if (logEnabled)	LogFMA("checkUpdate", "Comparing...", "Are equals");
 		
 			if (screenmode == 1) {				
