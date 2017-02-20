1636c8c2fdcade864b7f226e135d80863d830484
Robz8 <bobesh8@gmail.com>
2017-02-19 17:52:08 -0700

    Update TWLNAND side

--- twlnand-side/clean and make.bat
+++ twlnand-side/clean and make.bat
@@ -1,7 +1,6 @@
 @echo off
 make clean
 make
-patch_ndsheader_dsiware.py TWLapp-hb.nds --mode dsi --maker 01 --code TWLD --title "TWLOADER-TWL" --out TWLapp.nds
 make_cia --srl="TWLapp.nds"
-copy "TWLapp.cia" "../7zfile/sdroot/TWLoader - TWLNAND side.cia"
+copy "TWLapp.cia" "../7zfile/sdroot/_nds/twloader/cia/TWLoader - TWLNAND side.cia"
 pause
