2a3e87688384e9023d281de0e5a159489765cbce
Jolty <juplagon@gmail.com>
2017-02-22 21:31:47 +0100

    italian.h: improve translation
    By @nop90

--- gui/source/languages/italian.h
+++ gui/source/languages/italian.h
@@ -1,18 +1,16 @@
 #ifndef ITALIAN_H
 #define ITALIAN_H
-
 #include <3ds/types.h>
-
 // ITALIAN
 static const char *const lang_IT[STR_MAX] = {
-	
+     
     /** GUI **/
     "Ritorna al menu HOME",                                       // "Return to HOME Menu"
     "AVVIA",                                                   // "START" (used on cartridge indicator)
     "Nessuna scheda",                                       // "No cartridge"
     "Scheda sconosciuta",                                    // "Unknown Cartridge"
     "Impostazioni",                                               // "Settings"
-    "B： Indietro",                                               // "B: Back"   
+    "B：Chiudi",                                               // "B: Back"   
    
     /** Settings: GUI **/
     "Lingua",                                                   // "Language"
@@ -21,27 +19,24 @@ static const char *const lang_IT[STR_MAX] = {
     "Colore del menu",                                         // "Menu Color"
     "Visualizza il nome dei file",                                 // "Show filename"
     "Contatore giochi",                                           // "Game counter"
-    "Sfondo inferiore personalizzato",                       // "Custom bottom image"
-    "Aggiorna automaticamente bootstrap",                               // "Auto-update bootstrap"
+    "Sfonfo inferiore personalizzato",                       // "Custom bottom image"
+    "Aaggiorna automaticamente bootstrap",                               // "Auto-update bootstrap"
     "Aggiorna automaticamente TWLoader",                           // "Auto-update to latest TWLoader"
-	
     /** Settings: NTR/TWL_mode **/
     "Seleziona la Flashcard",                                   // "Flashcard(s) select"
     "LED arcobaleno",                                           // "Rainbow LED"
     "Velocità CPU ARM9",                                       // "ARM9 CPU Speed"
     "VRAM boost",                                               // "VRAM boost"
-    "Schermata di avvio DS/DSi",                                       // "DS/DSi Boot Screen"
+    "Schermata di avvioi DS/DSi",                                       // "DS/DSi Boot Screen"
     "Messaggio Salute e Sicurezza",                               // "Health and Safety message"
     "Resetta lo Slot-1",                                       // "Reset Slot-1"
     "Console output",                                           // "Console output"
     "Blocca ARM9 SCFG_EXT",                                       // "Lock ARM9 SCFG_EXT"
     "Bootstrap",                                               // "Bootstrap"
-	
     /** Settings: Top Screen **/
     "X: Aggiorna il bootstrap (Versione ufficiale)",           // "X: Update bootstrap (Official release)"
     "Y: Aggiorna il bootstrap (Versione non ufficiale)",       // "Y: Update bootstrap (Unofficial release)"
     "START: Aggiorna il TWLoader",                               // "START: Update TWLoader"
-	
     /** Settings: GUI values **/
     // Color
     "Grigio",                                                   // "Gray"
@@ -63,14 +58,12 @@ static const char *const lang_IT[STR_MAX] = {
     "Rosso e blu",                                               // "Red and blue"
     "Verde e giallo",                                           // "Green and yellow"
     "Natale",                                                   // "Christmas"
-	
     // Menu Color
     "Bianco",                                                   // "White"
     "Nero",                                                       // "Black"
-	
     /** Settings: GUI descriptions **/
     "La lingua usata per la UI,",                               // "The language to use for the UI,"
-    "incluso il testo dei banner dei giochi.",                   // "including game banner text."	
+    "incluso il testo dei banner dei giochi.",                   // "including game banner text."
     "Il tema usato con il TWLoader.",                               // "The theme to use in TWLoader."
     "Premi A per i sotto-temi.",                               // "Press A for sub-themes."
     "Il colore dello sfondo superiore, del bordo",                       // "The color of the top background,"
@@ -87,14 +80,13 @@ static const char *const lang_IT[STR_MAX] = {
     "all'avvio.",                                                         // (empty)
     "Scarica automaticamente l'ultima",                           // "Auto-download the CIA of the latest"
     "versione del CIA TWLoader  all'avvio.",                               // "TWLoader version at launch."
-	
     /** Settings: NTR/TWL_mode descriptions **/
     "Seleziona una flashcard da usare",                           // "Pick a flashcard to use to"
     "per avviare le ROM.",                                       // "run ROMs from it."
     "Mosta luci arcobaleno sul",                               // "See rainbow colors glowing in"
     "LED di notifica.",                                           // "the Notification LED."
-    "Imposta su TWL per eliminare",                       // "Set to TWL to get rid of lags in some games."
-    "il lag in alcuni giochi.",                                 // (empty)
+    "Impostastione affichè TWL elimini",                       // "Set to TWL to get rid of lags in some games."
+    "il lag su alcuni giochi.",                                                         // (empty)
      
     "Abilita la scrittura della VRAM a 8 bit",                   // "Allows 8 bit VRAM writes"
     "ed espande il bus a 32 bit.",                               // "and expands the bus to 32 bit."
@@ -110,7 +102,6 @@ static const char *const lang_IT[STR_MAX] = {
     "evitando conflitti con le recenti libnds.",               // "avoiding conflict with recent libnds."
     "Cabia tra versione ufficiale e",                           // "Change between release and"
     "non ufficiale del file di bootstrap.",                       // "unofficial bootstrap file."
-	
     /** Start menu **/
     // Options
     "Cartella giochi",                                           // "Game location"
@@ -119,36 +110,31 @@ static const char *const lang_IT[STR_MAX] = {
     "Avvia GBARunner2",                                         // "Start GBARunner2"
     "Bordo superiore: On",                                          // "Top border: On"
     "Bordo superiore: Off",                                         // "Top border: Off"
-    "Non usare ROM donor",                                  // "Unset donor rom"
+    "No ROM donatrice",                                         // "Unset donor rom"
     "Cerca",                                                   // "Search"
     // Values
     "Scheda SD",                                               // "SD Card"
     "Flashcard",                                               // "Flashcard"
     "Usa la tastiera per trovare le rom",                       // "Use the keyboard to find roms"
-	
     /** Select menu **/
     // Options
     "Velocità CPU ARM9",                                       // "ARM9 CPU Speed"
     "VRAM boost",                                               // "VRAM boost"
-    "Blocca SCFG_EXT ARM9",                                       // "Lock ARM9 SCFG_EXT"
-    "Imposta come ROM donor",                               // "Set as donor ROM"
-    "Imposta il colore del LED",                                   // "Set LED color"
+    "Blocca Scfg_Ext ARM9",                                       // "Lock ARM9 SCFG_EXT"
+    "Imposta ROM donatrice",                               // "Set as donor ROM"
+    "Imposta colore del LED",                                   // "Set LED color"
     // Values
     "Predefinito",                                               // "Default"
-	
     /** Sub-theme **/
     "Seleziona sotto-tema: DSi Menu",                           // "Sub-theme select: DSi Menu"
     "Seleziona sotto-tema: R4",                                   // "Sub-theme select: R4"
-    "Seleziona sotto-tema: akMenu/Wood",                               // "Sub-theme select: Wood"
+    "Seleziona sotto-tema: Wood",                               // "Sub-theme select: Wood"
     "Non esistono sotto-temi per questo tema.",                   // "No sub-themes exist for this theme."
-	
     /** Settings others minor strings **/
-    "A/B: Salva e torna a precedente",                           // "A/B: Save and Return"
+    "A/B: Salva a torna a precedente",                           // "A/B: Save and Return"
     "Left/Right: Seleziona",                                   // "Left/Right: Pick"
     "Impostazioni: GUI",                                            // "Settings: GUI"
     "Impostazioni: NTR/TWL-mode",                                   // "Settings: NTR/TWL-mode"
-	
+     
 };
-
-
 #endif /* ITALIAN_H */
