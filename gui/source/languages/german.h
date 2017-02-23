ea02245004cc0b7c4205cd6107027a8a300b6a4f
Jolty <juplagon@gmail.com>
2017-02-23 13:31:08 +0100

    languages: fix indentation

--- gui/source/languages/german.h
+++ gui/source/languages/german.h
@@ -5,15 +5,15 @@
 
 // GERMAN
 static const char *const lang_DE[STR_MAX] = {
-	
+
 	/** GUI **/
-    ": Zurück zum HOME-Menü",									// "Return to HOME Menu"
+	": Zurück zum HOME-Menü",									// "Return to HOME Menu"
 	"START",													// "START" (used on cartridge indicator)
 	"Keine Karte",												// "No cartridge"
 	"Unbekannte Karte",											// "Unknown Cartridge"
 	"Einstellungen",											// "Settings"
 	"B: Zurück",												// "B: Back"	
-	
+
 	/** Settings: GUI **/
 	"Sprache",													// "Language"
 	"Design",													// "Theme"
@@ -24,7 +24,7 @@ static const char *const lang_DE[STR_MAX] = {
 	"Benutzerdefiniertes unteres Bild",							// "Custom bottom image"
 	"Bootstrap automatisch updaten",							// "Auto-update bootstrap"
 	"TWLoader automatisch updaten",								// "Auto-update to latest TWLoader"
-	
+
 	/** Settings: NTR/TWL_mode **/
 	"Flashkart(en) auswählen",									// "Flashcard(s) select"
 	"Regenbogen LED",											// "Rainbow LED"
@@ -36,14 +36,14 @@ static const char *const lang_DE[STR_MAX] = {
 	"Konsolenausgabe",											// "Console output"
 	"ARM9 SCFG_EXT feststellen",								// "Lock ARM9 SCFG_EXT"
 	"Bootstrap",												// "Bootstrap"
-	
+
 	/** Settings: Top Screen **/
 	"X: Bootstrap updaten (Offizielle Version)",				// "X: Update bootstrap (Official release)"
 	"Y: Bootstrap updaten (Inoffizielle Version)",				// "Y: Update bootstrap (Unofficial release)"
 	"START: TWLoader aktualisieren",							// "START: Update TWLoader"
-	
+
 	/** Settings: GUI values **/
-	
+
 	// Color
 	"Grau",														// "Gray"
 	"Braun",													// "Brown"
@@ -72,62 +72,62 @@ static const char *const lang_DE[STR_MAX] = {
 	/** Settings: GUI descriptions **/
 	"Die Sprache der Benutzeroberfläche",						// "The language to use for the UI,"
 	"und der Spielenamen.",										// "including game banner text."
-	
+
 	"Das benutzte Design für TWLoader.",						// "The theme to use in TWLoader."
 	"Drücke A für Unterdesigns.",								// "Press A for sub-themes."
-	
+
 	"Die Farbe des oberen Hintergrunds,",						// "The color of the top background,"
 	"der START Umrandung und der kreisenden Punkte.",			// "the START border, and the circling dots."
-	
+
 	"Die Farbe des oberen Randes",								// "The color of the top border,"
 	"und des unteren Hintergrundes.",							// "and the bottom background."
-	
+
 	"Zeigt den Dateinamen des Spiels oben in der Blase.",		// "Shows game filename at the top of the bubble."
 	"",															// (empty)
 
 	"Die Nummer des ausgewählten Spiels und die",				// "A number of selected game and listed games"
 	"Anzahl der Spiele wird unter der Textblase angezeigt.",	// "is shown below the text bubble."
-	
+
 	"Läd ein benutzerdefiniertes Bild für den unteren",			// "Loads a custom bottom screen image"
 	"Bildschirm im Spiele-Menü.",								// "for the game menu."
-	
+
 	"nds-bootstrap beim Start automatisch updaten.",			// "Auto-update nds-bootstrap at launch."
 	"",															// (empty)
 
 	"Die neuste TWLoader Version beim Start",					// "Auto-download the CIA of the latest"
 	"automatisch herunterladen.",								// "TWLoader version at launch."
-	
+
 	/** Settings: NTR/TWL_mode descriptions **/
 	"Wähle die Flashkarte, die du zum",							// "Pick a flashcard to use to"
 	"ausführen von ROMs benutzen willst.",						// "run ROMs from it."
-	
+
 	"Aktiviere Regenbogen Farben für",							// "See rainbow colors glowing in"
 	"die Benachrichtigungs-LED.",								// "the Notification LED."
-	
+
 	"Stelle auf TWL, um Lags in",								// "Set to TWL to get rid of lags in some games."
 	"manchen Spielen zu vermeiden.",							// (empty)
 
 	"Erlaubt 8 bit VRAM Schreibvorgänge",						// "Allows 8 bit VRAM writes"
 	"und erweitert den Bus auf 32 bit.",						// "and expands the bus to 32 bit."
-	
+
 	"Zeigt die DS/DSi Boot-Animation",							// "Displays the DS/DSi boot animation"
 	"vor dem Spielstart an.",									// "before launched game."
-	
+
 	"Zeigt die Gesundheits- und Sicherheits-",					// "Displays the Health and Safety"
 	"warnung auf dem unteren Bildschirm an.",					// "message on the bottom screen."
-	
+
 	"Aktivieren, wenn Slot-1 Karten auf",						// "Enable this if Slot-1 carts are stuck"
 	"einem weißen Bildschirm hängen bleiben.",					// "on white screens."
-	
+
 	"Zeigt etwas Text vor dem Spielstart an.",					// "Displays some text before launched game."
 	"",															// (empty)
 
 	"Stellt den ARM9 SCFG_EXT fest,",							// "Locks the ARM9 SCFG_EXT,"
 	"vermeidet Konflikt mit aktuellem libnds",					// "avoiding conflict with recent libnds."
-	
+
 	"Wechsele zwischen offizieller",							// "Change between release and"
 	"und inoffizieller Bootstrap Version",						// "unofficial bootstrap file."
-	
+
 	/** Start menu **/
 	// Options
 	"Spielort",													// "Game location"
@@ -142,7 +142,7 @@ static const char *const lang_DE[STR_MAX] = {
 	"SD Karte",													// "SD Card"
 	"Flashkarte",												// "Flashcard"
 	"Benutze die Tastatur um ROMs zu finden",					// "Use the keyboard to find roms"
-	
+
 	/** Select menu **/
 	// Options
 	"CPU Geschwindigkeit",										// "ARM9 CPU Speed" (Removed ARM9, because it did not fit inside the box)
@@ -152,14 +152,14 @@ static const char *const lang_DE[STR_MAX] = {
 	"LED Farbe",												// "Set LED color"
 	// Values
 	"Standart",													// "Default"
-	
+
 	/** Sub-theme **/
 	"Unterdesignauswahl: DSi Menu",								// "Sub-theme select: DSi Menu"
 	"Unterdesignauswahl: R4",									// "Sub-theme select: R4"
 	"Unterdesignauswahl: akMenu/Wood",							// "Sub-theme select: akMenu/Wood"
-	
+
 	"Für dieses Design existiert kein Unterdesign.",			// "No sub-themes exist for this theme."
-	
+
 	/** Settings others minor strings **/
 	"A/B: Speichern und Zurück",								// "A/B: Save and Return"
 	"Links/Rechts: Auswählen",									// "Left/Right: Pick"
--- gui/source/languages/spanish.h
+++ gui/source/languages/spanish.h
@@ -5,15 +5,15 @@
 
 // SPANISH
 static const char *const lang_ES[STR_MAX] = {
-	
+
 	/** GUI **/
-    ": Volver al menú HOME",									// "Return to HOME Menu"
-	"INICIAR",													// "START" (used on cartridge indicator)tor)
+	": Volver al menú HOME",									// "Return to HOME Menu"
+	"INICIAR",													// "START" (used on cartridge indicator)
 	"Sin cartucho",												// "No cartridge"
 	"Cartucho desconocido", 									// "Unknown Cartridge"
 	"Ajustes",													// "Settings"
 	"B: Volver",												// "B: Back"	
-	
+
 	/** Settings: GUI **/
 	"Idioma",													// "Language"
 	"Tema",														// "Theme"
@@ -24,7 +24,7 @@ static const char *const lang_ES[STR_MAX] = {
 	"Usar imagen personalizada",								// "Custom bottom image"
 	"Auto actualizar bootstrap",								// "Auto-update bootstrap"
 	"Auto actualizar TWLoader",									// "Auto-update to latest TWLoader"
-	
+
 	/** Settings: NTR/TWL_mode **/
 	"Seleccionar flashcard",									// "Flashcard(s) select"
 	"LED arcoiris",												// "Rainbow LED"
@@ -36,14 +36,14 @@ static const char *const lang_ES[STR_MAX] = {
 	"Salida de consola",										// "Console output"
 	"Bloquear el SCFG_EXT del ARM9",							// "Lock ARM9 SCFG_EXT"
 	"Bootstrap",												// "Bootstrap"
-	
+
 	/** Settings: Top Screen **/
 	"X: Actualizar bootstrap (Versión estable)",				// "X: Update bootstrap (Official release)"
 	"Y: Actualizar bootstrap (Versión inestable)",				// "Y: Update bootstrap (Unofficial release)"
 	"START: Actualizar TWLoader",								// "START: Update TWLoader"
-	
+
 	/** Settings: GUI values **/
-	
+
 	// Color
 	"Gris",														// "Gray"
 	"Marrón",													// "Brown"
@@ -72,59 +72,59 @@ static const char *const lang_ES[STR_MAX] = {
 	/** Settings: GUI descriptions **/
 	"El idioma para usar en TWLoader.",							// "The language to use for the UI,"
 	"(también afecta al banner de las roms)",					// "including game banner text."
-	
+
 	"El tema para TWLoader.",									// "The theme to use in TWLoader."
 	"Pulsa A para elegir un sub tema.",							// "Press A for sub-themes."
-	
+
 	"El color del fondo de la pantalla superior, el marco",		// "The color of the top background,"
 	"de selección de ROMs y el círculo de puntos.",				// "the START border, and the circling dots."
-	
+
 	"Color del marco superior,",								// "The color of the top border,"
 	"y del fondo de la pantalla inferior.",						// "and the bottom background."
-	
+
 	"Muestra el nombre del archivo en la parte ", 				// "Shows game filename at the top of the bubble."
 	"superior de la burbuja de información",					// (empty)
-	
+
 	"Muestra el número de la ROM selecionada y el",				// "A number of selected game and listed games"
 	"total debajo de la burbuja de información.",				// "is shown below the text bubble."
-	
+
 	"Carga una imagen personalizada para",						// "Loads a custom bottom screen image"
 	"la pantalla inferior.",									// "for the game menu."
-	
+
 	"Auto actualizar bootstrap durante el arranque.",			// "Auto-update nds-bootstrap at launch."
 	"",															// (empty) 
-	
+
 	"Auto actualizar e installar los CIAS de TWLoader",			// "Auto-download the CIA of the latest"
 	"durante el arranque.",										// "TWLoader version at launch."
-	
+
 	/** Settings: NTR/TWL_mode descriptions **/
 	"Elije una flashcard para cargar",							// "Pick a flashcard to use to"
 	"ROMs desde ella.",											// "run ROMs from it."
-	
+
 	"Mostrar un efecto arcoiris de colores",					// "See rainbow colors glowing in"
 	"en el LED de notificaciones.",								// "the Notification LED."
-	
+
 	"Selecciona el modo TWL para eliminar ",					// "Set to TWL to get rid of lags in some games."
 	"el lag en algunas ROMs.",									// (empty) 
 
 	"Permite escribir en la VRAM 8 bit",						// "Allows 8 bit VRAM writes"
 	"y expandir el bus de datos a 32 bits.",					// "and expands the bus to 32 bit."
-	
+
 	"Muestra la animación de arranque de la DS/DSi",			// "Displays the DS/DSi boot animation"
 	"antes de ejecutar la ROM.",								// "before launched game."
-	
+
 	"Muestra el mensaje de Salud y Seguridad",					// "Displays the Health and Safety"
 	"en la pantalla inferior.",									// "message on the bottom screen."
-	
+
 	"Activa esta opción para reiniciar el Slot-1",				// "Enable this if Slot-1 carts are stuck"
 	"si el cartucho se queda con pantallas blancas.",			// "on white screens."
-	
+
 	"Muestra información de depuración",						// "Displays some text before launched game."
 	"antes de lanzar la ROM.",									// (empty) 
 
 	"Bloquea el SCFG_EXT del ARM9, evitando",					// "Locks the ARM9 SCFG_EXT,"
 	"conflictos con las librerias libnds más recientes.",		// "avoiding conflict with recent libnds."
-	
+
 	"Cambia entre la version 'Release' (estable) ",				// "Change between release and"
 	"y la 'Unofficial' (inestable).",							// "unofficial bootstrap file."
 
@@ -142,7 +142,7 @@ static const char *const lang_ES[STR_MAX] = {
 	"Tarjeta SD",												// "SD Card"
 	"Flashcard",												// "Flashcard"
 	"Usa el teclado para buscar ROMs",							// "Use the keyboard to find roms"
-	
+
 	/** Select menu **/
 	// Options
 	"Velocidad CPU ARM9:",										// "ARM9 CPU Speed"
@@ -152,14 +152,14 @@ static const char *const lang_ES[STR_MAX] = {
 	"Elegir color del LED",										// "Set LED color"
 	// Values
 	"Por defecto",												// "Default"
-	
+
 	/** Sub-theme **/
 	"Sub tema seleccionado: Menu DSi",							// "Sub-theme select: DSi Menu"
 	"Sub tema seleccionado: R4",								// "Sub-theme select: R4"
 	"Sub tema seleccionado: akMenu/Wood",						// "Sub-theme select: Wood"
-	
+
 	"No hay sub temas para este tema.",							// "No sub-themes exist for this theme."
-	
+
 	/** Settings others minor strings **/
 	"A/B: Guardar y volver",									// "A/B: Save and Return"
 	"Izquierda/Derecha: Seleccionar",							// "Left/Right: Pick"
