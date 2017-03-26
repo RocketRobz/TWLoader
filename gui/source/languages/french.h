af9243b33c6c08235b1308c8af9764874a4a61a5
daffyb <daffyb@free.fr>
2017-03-26 19:34:00 +0200

    Update french.h
    Some translation corrections (I am French).
    Some texts were too long to fit on the display

--- gui/source/languages/french.h
+++ gui/source/languages/french.h
@@ -21,10 +21,10 @@ static const char *const lang_FR[STR_MAX] = {
 	"Couleur",                                                  // "Color"
 	"Menu des couleurs",                                        // "Menu Color"
 	"Afficher le nom du fichier",                               // "Show filename"
-	"Conteur de jeux",                                          // "Game counter"
+	"Compteur de jeux",                                          // "Game counter"
 	"Image du bas personnalisée",                               // "Custom bottom image"
-	"Mise à jour auto. de la bootstrap",                        // "Auto-update bootstrap"
-	"Mise à jour auto. de TWLoader",                           	// "Auto-update to latest TWLoader"
+	"Mise à jour auto. bootstrap",                        // "Auto-update bootstrap"
+	"Mise à jour auto. TWLoader",                           	// "Auto-update to latest TWLoader"
 	 
 	/** Settings: NTR/TWL_mode **/
 	"Sélection du linker(s)",                                   // "Flashcard(s) select"
@@ -33,14 +33,14 @@ static const char *const lang_FR[STR_MAX] = {
 	"Boost VRAM",                                               // "VRAM boost"
 	"Écran de démarrage DS/DSi",                                // "DS/DSi Boot Screen"
 	"Message de Santé et Sécurité",                             // "Health and Safety message"
-	"Re-lancé Slot-1",                                          // "Reset Slot-1"
-	"Affichage",                                           		// "Console output"
-	"Vérouiller SCFG_EXT ARM9",                                 // "Lock ARM9 SCFG_EXT"
+	"Reset Slot-1",                                          // "Reset Slot-1"
+	"Affichage Console",                                           		// "Console output"
+	"Verrrouiller SCFG_EXT ARM9",                                 // "Lock ARM9 SCFG_EXT"
 	"Bootstrap",                                                // "Bootstrap"
 	 
 	/** Settings: Top Screen **/
-	": Mise à jour de la bootstrap (Release officiel)",        // "X: Update bootstrap (Official release)"
-	": Mise à jour de la bootstrap (Release non-officiel)",    // "Y: Update bootstrap (Unofficial release)"
+	": Mise à jour du bootstrap (release)",        // "X: Update bootstrap (Official release)"
+	": Mise à jour du bootstrap (non-officiel)",    // "Y: Update bootstrap (Unofficial release)"
 	"START: Mettre à jour TWLoader",                            // "START: Update TWLoader"
 	 
 	/** Settings: GUI values **/
@@ -55,16 +55,16 @@ static const char *const lang_FR[STR_MAX] = {
 	"Jaune-Vert",                                            	// "Yellow-Green"
 	"Vert 1",                                                  	// "Green 1"
 	"Vert 2",                                                  	// "Green 2"
-	"Vert lumière",                                             // "Light green"
+	"Vert clair",                                             // "Light green"
 	"Bleu ciel",                                                // "Sky blue"
-	"Bleu lumière",                                             // "Light blue"
+	"Bleu clair",                                             // "Light blue"
 	"Bleu",                                                     // "Blue"
 	"Violet",                                                   // "Violet"
 	"Pourpre",                                                  // "Purple"
 	"Fuchsia",                                                  // "Fuchsia"
 	"Rouge&Bleu",                                               // "Red and blue"
 	"Jaune&Vert",                                             	// "Green and yellow"
-	"Noel",                                                		// "Christmas"
+	"Noël",                                                		// "Christmas"
 
 	// Menu Color
 	"Blanc",                                                    // "White"
@@ -86,8 +86,8 @@ static const char *const lang_FR[STR_MAX] = {
 	"Afficher le nom du fichier du jeu dans ",  				// "Shows game filename at the top of the bubble."
 	"le bas de la bulle.",                                      // (empty)
 	 
-	"Un numéro de jeux sélectionner et lister",              	// "A number of selected game and listed games"
-	"est afficher au dessous de la bulle de texte.",            // "is shown below the text bubble."
+	"Un numéro de jeux sélectionné et listé",              	// "A number of selected game and listed games"
+	"est affiché au dessous de la bulle de texte.",            // "is shown below the text bubble."
 	 
 	"Charge une image personnalisée sur l'écran du bas",        // "Loads a custom bottom screen image"
 	"dans le menu des jeux.",                                   // "for the game menu."
@@ -95,21 +95,21 @@ static const char *const lang_FR[STR_MAX] = {
 	"Mise à jour auto. de nds-bootstrap au démarrage.",         // "Auto-update nds-bootstrap at launch."
 	"",                                                         // (empty)
 
-	"Téléchargez automatiquement le dernier CIA ",              // "Auto-download the CIA of the latest"
+	"Télécharger automatiquement le dernier CIA ",              // "Auto-download the CIA of the latest"
 	"de TWLoader au démarrage.",                              	// "TWLoader version at launch."
 	 
 	/** Settings: NTR/TWL_mode descriptions **/
 	"Sélectionnez un linker à utiliser pour",                   // "Pick a flashcard to use to"
 	"lancer les ROMs.",                                        	// "run ROMs from it."
 	 
-	"Afficher les couleurs de l'arc-en-ciel clignotante dans",  // "See rainbow colors glowing in"
+	"Afficher un arc-en-ciel clignotant dans",  // "See rainbow colors glowing in"
 	"la LED de notification.",                                  // "the Notification LED."
 	 
-	"Définis à TWL pour se débarrasser de lags ",             	// "Set to TWL to get rid of lags in some games."
+	"Sélectionner pour se débarrasser de lags ",             	// "Set to TWL to get rid of lags in some games."
 	"dans certains jeux.",                                      // (empty)
 	 
-	"Permet l'écriture VRAM 8 bit",                             // "Allows 8 bit VRAM writes"
-	"et étandre le bus de 32 bit.",                           	// "and expands the bus to 32 bit."
+	"Permettre l'écriture VRAM 8 bit",                             // "Allows 8 bit VRAM writes"
+	"et étendre le bus de 32 bit.",                           	// "and expands the bus to 32 bit."
 	 
 	"Afficher l'animation de démarrage de la DS/DSi",           // "Displays the DS/DSi boot animation"
 	"avant le jeu lancé.",                                    	// "before launched game."
@@ -117,13 +117,13 @@ static const char *const lang_FR[STR_MAX] = {
 	"Afficher le message de Santé et Sécurité",                 // "Displays the Health and Safety"
 	"sur l'écran du bas.",                            			// "message on the bottom screen."
 	 
-	"Activer ceci si les cartouches Slot-1 sont coincé",        // "Enable this if Slot-1 carts are stuck"
+	"Activer si les cartouches Slot-1 sont coincées",        // "Enable this if Slot-1 carts are stuck"
 	"sur des écrans blancs.",                                   // "on white screens."
 	 
 	"Aficher du texte avant de lancer un jeu.",                 // "Displays some text before launched game."
 	"",                                                         // (empty)
 	 
-	"Verouille le SCFG_EXT de l'ARM9,",                         // "Locks the ARM9 SCFG_EXT,"
+	"Verrouille le SCFG_EXT de l'ARM9,",                         // "Locks the ARM9 SCFG_EXT,"
 	"évitant les conflits avec la libnds récente libnds.",      // "avoiding conflict with recent libnds."
 	 
 	"Changer de fichier de bootstrap (release et",              // "Change between release and"
@@ -148,7 +148,7 @@ static const char *const lang_FR[STR_MAX] = {
 	// Options
 	"Vitesse du CPU ARM9",                                      // "ARM9 CPU Speed"
 	"Boost VRAM",                                               // "VRAM boost"
-	"Vérouiller SCFG_EXT ARM9",                                 // "Lock ARM9 SCFG_EXT"
+	"Verrouiller SCFG_EXT ARM9",                                 // "Lock ARM9 SCFG_EXT"
 	"Définir comme donor ROM",                                  // "Set as donor ROM"
 	"Définir la couleur de la LED",                             // "Set LED color"
 	// Values
@@ -159,7 +159,7 @@ static const char *const lang_FR[STR_MAX] = {
 	"Sélection du sous-thème: R4",                              // "Sub-theme select: R4"
 	"Sélection du sous-thème: akMenu/Wood",                     // "Sub-theme select: Wood"
 	 
-	"Aucune autre sous-thèmes pour ce thème.",                  // "No sub-themes exist for this theme."
+	"Aucun autre sous-thèmes pour ce thème.",                  // "No sub-themes exist for this theme."
 	 
 	/** Settings others minor strings **/
 	"/: Sauvegarder and Retourner",                           // "A/B: Save and Return"
