501404a223acd73ee9ebb85350b42a2b444b45ed
Jolty <Jolty95@users.noreply.github.com>
2017-05-04 18:55:14 +0200

    compile.bat: update to point at new submodule

--- gui/compile_3dsx.bat
+++ gui/compile_3dsx.bat
@@ -2,7 +2,7 @@
 make clean
 make COMPILE_3DSX=1 3dsx
 copy "TWLoader.3dsx" "../7zfile/sdroot/3ds/TWLoader/TWLoader.3dsx"
-copy "TWLoader.3dsx" "../../TWLoader-update/TWLoader.3dsx"
+copy "TWLoader.3dsx" "../TWLoader-update/TWLoader.3dsx"
 copy "TWLoader.smdh" "../7zfile/sdroot/3ds/TWLoader/TWLoader.smdh"
-copy "TWLoader.smdh" "../../TWLoader-update/TWLoader.smdh"
+copy "TWLoader.smdh" "../TWLoader-update/TWLoader.smdh"
 pause
