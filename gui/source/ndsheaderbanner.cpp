de5952c8b4c76fd0f81ec09f8aeba7d3ee416757
Robz8 <bobesh8@gmail.com>
2017-10-26 19:40:44 -0600

    somewhat fix colors

--- gui/source/ndsheaderbanner.cpp
+++ gui/source/ndsheaderbanner.cpp
@@ -33,6 +33,20 @@ GX_TRANSFER_FORMAT gpuToGxFormat[13] = {
 };
 
 /**
+ * Convert a color from NDS BGR555 to RGB5A1.
+ * @param px16 BGR555 color value.
+ * @return RGB5A1 color.
+ */
+static inline u16 BGR555_to_RGB5A1(u16 px16) {
+	// BGR555: xBBBBBGG GGGRRRRR
+	// RGB565: RRRRRGGG GGGBBBBB
+	// RGB5A1: RRRRRGGG GGBBBBBA
+	return   (px16 << 11) |	1 |		// Red (and alpha)
+		((px16 <<  1) & 0x07C0) |	// Green
+		((px16 >>  9) & 0x003E);	// Blue
+}
+
+/**
  * Get the title ID.
  * @param ndsFile DS ROM image.
  * @param buf Output buffer for title ID. (Must be at least 4 characters.)
@@ -281,6 +295,17 @@ static struct {
  * @return Icon texture. (NULL on error)
  */
 void* grabIcon(const sNDSBanner* ndsBanner) {
+	// Convert the palette from BGR555 to RGB5A1.
+	// (We need to ensure the MSB is set for all except
+	// color 0, which is transparent.)
+	u16 palette[16];
+	palette[0] = 0;	// Color 0 is always transparent.
+	for (int i = 16-1; i > 0; i--) {
+		// Convert from NDS BGR555 to RGB5A1.
+		// NOTE: The GPU expects byteswapped data.
+		palette[i] = BGR555_to_RGB5A1(ndsBanner->palette[i]);
+	}
+
 	static const int width = 32;
 	static const int height = 32;
 	u8 icon[32 * 32 * 2];
@@ -288,7 +313,7 @@ void* grabIcon(const sNDSBanner* ndsBanner) {
 		for(u32 y = 0; y < 32; y++) {
 			u32 srcPos = (((y >> 3) * 4 + (x >> 3)) * 8 + (y & 7)) * 4 + ((x & 7) >> 1);
 			u32 srcShift = (x & 1) * 4;
-			u16 srcPx = ndsBanner->palette[(ndsBanner->icon[srcPos] >> srcShift) & 0xF];
+			u16 srcPx = palette[(ndsBanner->icon[srcPos] >> srcShift) & 0xF];
 
 			u8 r = (u8) (srcPx & 0x1F);
 			u8 g = (u8) ((srcPx >> 5) & 0x1F);
